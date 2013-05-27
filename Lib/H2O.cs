8d09b71c6cbd0181fcb7f23737bf4766866c064c
M_J_O_N_E_S <M_J_O_N_E_S@hotmail.com>
2013-05-27 21:55:59 +0100

    Prefactor

---
+++ Lib/H2O.cs
@@ -0,0 +1,49 @@
+﻿namespace H2OLib
+{
+    public class H2O
+    {
+        public enum State
+        {
+            Gas,
+            Liquid,
+            Solid
+        }
+
+        private readonly State _state;
+
+        public H2O(State state)
+        {
+            _state = state;
+        }
+
+        public int MaxTemp
+        {
+            get
+            {
+                switch (_state)
+                {
+                    case State.Gas:
+                        return 374;
+                    case State.Liquid:
+                        return 100;
+                    default:
+                        return 0;
+                }
+            }
+        }
+
+        public int MinTemp
+        {
+            get{
+            switch (_state)
+            {
+                case State.Gas:
+                    return 100;
+                case State.Liquid:
+                    return 0;
+                default:
+                    return -230;
+            }}
+        }
+    }
+}
---
+++ Tests/H2OTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace H2OLib.Test
+{
+    [TestFixture]
+    public class H2OTests
+    {
+        [Test]
+        public void GivenGasStateThenExpectCorrectTemps()
+        {
+            var h2o = new H2O(H2O.State.Gas);
+            Assert.That(h2o.MinTemp, Is.EqualTo(100));
+            Assert.That(h2o.MaxTemp, Is.EqualTo(374));
+        }
+
+        [Test]
+        public void GivenLiquidStateThenExpectCorrectTemps()
+        {
+            var h2o = new H2O(H2O.State.Liquid);
+            Assert.That(h2o.MinTemp, Is.EqualTo(0));
+            Assert.That(h2o.MaxTemp, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void GivenSolidStateThenExpectCorrectTemps()
+        {
+            var h2o = new H2O(H2O.State.Solid);
+            Assert.That(h2o.MinTemp, Is.EqualTo(-230));
+            Assert.That(h2o.MaxTemp, Is.EqualTo(0));
+        }
+    }
+}
