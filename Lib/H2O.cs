6ec13ccc79d6f7bdec1e938cd20df7e56bccfd37
M_J_O_N_E_S <M_J_O_N_E_S@hotmail.com>
2013-05-28 08:24:37 +0100

    Combined to single code file

--- Lib/H2O.cs
+++ Lib/H2O.cs
@@ -35,4 +35,74 @@ namespace H2OLib
             }
         }
     }
+
+    public interface IState
+    {
+        int MinTemp { get; }
+        int MaxTemp { get; }
+    }
+
+    public class StateFactory
+    {
+        public IState CreateState(H2OLib.H2O.State state)
+        {
+            switch (state)
+            {
+                case H2O.State.Gas:
+                    {
+                        return new GasState();
+                    }
+                case H2O.State.Liquid:
+                    {
+                        return new LiquidState();
+                    }
+                case H2O.State.Solid:
+                    {
+                        return new SolidState();
+                    }
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    public class GasState : IState
+    {
+        public int MinTemp
+        {
+            get { return 100; }
+        }
+
+        public int MaxTemp
+        {
+            get { return 374; }
+        }
+    }
+
+    public class LiquidState : IState
+    {
+        public int MinTemp
+        {
+            get { return 0; }
+        }
+
+        public int MaxTemp
+        {
+            get { return 100; }
+        }
+    }
+
+    public class SolidState : IState
+    {
+        public int MinTemp
+        {
+            get { return -230; }
+        }
+
+        public int MaxTemp
+        {
+            get { return 0; }
+        }
+    }
+
 }
--- Lib/IState.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace H2OLib
-{
-    public interface IState
-    {
-        int MinTemp { get; }
-        int MaxTemp { get; }
-    }
-}
